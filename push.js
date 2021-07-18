83bc7a83b3c9a3e9f2e89feef8dbc38736623baf
matthias müller <mwm@cephei.com>
2021-07-18 17:40:37 +0200

    teste

--- push.js
+++ push.js
@@ -1,14 +1,18 @@
 var push = require('web-push');
+/*
+let xxcsdv = push.generateVAPIDKeys();
+console.log(xxcsdv)
+*/
 
 
 let vapidKeys = {
-    publickey: "BDnwSN2FFnVXIDjDZyb_ztH6_PdR0rnP8NL8GSSbRn8aMBNFRRNIFv0kOOHOP_MyK8J-z1-luVmVepYMvoFaZpc",
-    privateKey: "ZavvhFQoTjW9aiHWBxeGc-VvLnxFbtm7TCERBHlHgNk"
+    publicKey: "BJiV7ZcIJnccbe1NuWri9EzXu6qCq3Zr7PbWB-4udPgPP--t1MfUoT1BHSKOwO_MPou05kJOCz5t-CfMXgpjxQc-z1-luVmVepYMvoFaZpc",
+    privateKey: "CxRxvKffw_DAeUh1Awwhnx8vOQVPxqxm1tUkCtPsuaA-VvLnxFbtm7TCERBHlHgNk"
 }
 
 
 
-push.setVapidDetails('mailto:test@code.ik.de', vapidKeys.publickey, vapidKeys.privateKey)
+push.setVapidDetails('mailto:test@code.ik.de', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub = {};
 push.sendNotification(sub, "test")
